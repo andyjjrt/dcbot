0e286040c0d42f4c5aa4a72411504f1f7572b2a6
andyjjrt <andyjjrt@gmail.com>
2023-03-30 19:45:17 +0800

    fix(Setting): Only admins can use setting

--- src/commands/setting.ts
+++ src/commands/setting.ts
@@ -1,5 +1,5 @@
-import { SuccessEmbed } from './../utils/Embed';
-import { SlashCommandBuilder, CommandInteraction } from "discord.js";
+import { ErrorEmbed, SuccessEmbed } from './../utils/Embed';
+import { SlashCommandBuilder, CommandInteraction, PermissionsBitField } from "discord.js";
 import { client } from "../index";
 import { Setting } from '../utils/db/schema';
 
@@ -11,13 +11,17 @@ export default {
       option.setName("ytkey").setDescription("Youtube API Key").setRequired(true)
     ),
   async execute(interaction: CommandInteraction) {
-    await interaction.deferReply({ephemeral: true});
+    await interaction.deferReply({ ephemeral: true });
+    const isAdmin = (interaction.member!.permissions as PermissionsBitField).has(PermissionsBitField.Flags.Administrator);
+    if(!isAdmin) {
+      interaction.followUp({ embeds: [new ErrorEmbed(interaction.client, "Error", "You don't have permission to do so.")], ephemeral: true });
+    }
     const guildId = interaction.guildId || "";
     const ytKey = interaction.options.get("ytkey", true).value as string;
     await Setting.upsert({
       guildId: guildId,
       ytKey: ytKey,
     });
-    await interaction.followUp({ embeds: [new SuccessEmbed(interaction.client, "Success", `ytKet changed to \`${ytKey}\``)], ephemeral: true });
+    await interaction.followUp({ embeds: [new SuccessEmbed(interaction.client, "Success", `ytKet changed to || \`${ytKey}\` ||`)], ephemeral: true });
   },
 };
