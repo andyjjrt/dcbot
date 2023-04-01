1d100065acbf3a3f2de6966092a679df2edf6dbb
andyjjrt <andyjjrt@gmail.com>
2023-04-01 11:24:08 +0800

    feat: Use voice ping if in guild voice channel

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,21 @@
 import { InfoEmbed } from './../utils/Embed';
 import { SlashCommandBuilder, CommandInteraction } from "discord.js";
-import { client } from "../index";
+import { client, subscriptions } from "../index";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Simple ping pong game"),
   async execute(interaction: CommandInteraction) {
-    await interaction.reply({ embeds: [new InfoEmbed(interaction.client.user, ":ping_pong:  Pong", `Ball flew back in ${client.ws.ping}ms`)] });
+    let subscription = subscriptions.get(interaction.guildId!);
+    await interaction.reply({
+      embeds: [
+        new InfoEmbed(
+          interaction.client.user,
+          `:ping_pong:  ${subscription?.voiceConnection.ping.ws ? "Voice" : "API"} Pong`,
+          `Ball flew back in ${subscription?.voiceConnection.ping.ws ||client.ws.ping}ms`
+        )
+      ]
+    });
   },
 };
