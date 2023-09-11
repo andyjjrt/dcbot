2d34d0d17f5190ba62758f219ac4680ea2553903
andyjjrt <andyjjrt@gmail.com>
2023-09-11 11:00:12 +0800

    feat: hypixel is a good game

---
+++ src/commands/hypixel.ts
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder, CommandInteraction, time } from "discord.js";
+import { SuccessEmbed } from "../utils/Embed";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("hypixel")
+    .setDescription("Which gives fun fact"),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply({
+      embeds: [
+        new SuccessEmbed(
+          interaction.client.user,
+          "Hypixel is a good game",
+          `
+          andyjjrt: ${time(new Date("2024-09-07T17:29:38.000"), "R")}(${time(new Date("2024-09-07T17:29:38.000"), "F")})
+          watamelonnnn: ${time(new Date("2023-09-13T07:39:49.000"), "R")}(${time(new Date("2023-09-13T07:39:49.000"), "F")})
+          murasakishionnnn: ${time(new Date("2024-08-12T06:59:45.000"), "R")}(${time(new Date("2024-08-12T06:59:45.000"), "F")})
+          `
+        ),
+      ],
+    });
+  },
+};
