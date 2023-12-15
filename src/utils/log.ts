28d94139ea7d2bd8dcc3696a1355936995776461
andyjjrt <andyjjrt@gmail.com>
2023-12-16 00:37:35 +0800

    fix: elk log cert

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,14 +1,19 @@
 import pino from "pino";
 import { multistream } from "pino";
 import pinoElastic from "pino-elasticsearch";
+import fs from "fs";
 
-const { ELK_INDEX, ELK_HOST, ELK_APIKEY } = process.env;
+const { ELK_INDEX, ELK_HOST, ELK_APIKEY, ELK_CERT } = process.env;
 
 const streamToElastic = pinoElastic({
   index: ELK_INDEX || "dcbot",
   node: ELK_HOST || "",
   auth: {
     apiKey: ELK_APIKEY || ""
+  },
+  tls: {
+    ca: fs.readFileSync(ELK_CERT || ""),
+    rejectUnauthorized: false
   },
   esVersion: 8,
   flushBytes: 1000,
