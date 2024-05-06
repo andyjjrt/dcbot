a1bec319766a85f88d31b3130e5bf8a962dc91bb
andyjjrt <andyjjrt@gmail.com>
2024-05-06 11:07:36 +0800

    fix: tmp disable command

---
+++ src/commands/hypixel.disabled.ts
@@ -0,0 +1,28 @@
+import { SlashCommandBuilder, CommandInteraction, time, ChannelType } from "discord.js";
+import { SuccessEmbed } from "../utils/Embed";
+import { request } from "undici";
+
+let counter = 0;
+
+const testTimer = setInterval(() => {
+  counter++;
+}, 1000);
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("worm")
+    .setDescription("Announce worm price at")
+    .addChannelOption((option) =>
+      option
+        .setName("channel")
+        .setDescription("The channel to echo into")
+        .addChannelTypes(ChannelType.GuildText)
+    ),
+  async execute(interaction: CommandInteraction) {
+    const data = await request(`https://api.hypixel.net/v2/skyblock/bazaar`).then((res) => res.body.json());
+    await interaction.reply({
+      embeds: [new SuccessEmbed(interaction.user, "1123", JSON.stringify((data as any).products.WORM_MEMBRANE.quick_status))],
+      ephemeral: true,
+    });
+  },
+};
